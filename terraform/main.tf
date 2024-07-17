5c47ee97dea637fc44d6337a65f3ff6582d66814
Leo Gomez <leo.gomez@ackstorm.com>
2024-07-17 20:57:11 +0200

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "rg" {
   name     = "casopractico2-lgc2--rg"
-  location = "West Europe"
+  location = "North Europe"
 }
 
 resource "azurerm_container_registry" "acr" {
@@ -9,6 +9,8 @@ resource "azurerm_container_registry" "acr" {
   location            = azurerm_resource_group.rg.location
   sku                 = "Basic"
   admin_enabled       = true
+
+  depends_on = [azurerm_resource_group.rg]
 }
 
 resource "azurerm_virtual_network" "vnet" {
@@ -16,6 +18,8 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   address_space       = ["10.0.0.0/16"]
+
+  depends_on = [azurerm_resource_group.rg]
 }
 
 resource "azurerm_subnet" "subnet" {
@@ -23,6 +27,8 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
+
+  depends_on = [azurerm_virtual_network.vnet]
 }
 
 resource "azurerm_public_ip" "public_ip" {
@@ -30,10 +36,32 @@ resource "azurerm_public_ip" "public_ip" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Dynamic"
+
+  depends_on = [azurerm_resource_group.rg]
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  depends_on = [azurerm_resource_group.rg]
 }
 
 resource "azurerm_network_interface" "nic" {
-  name                = "nic1"
+  name                = "nic-lgc"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
@@ -43,6 +71,19 @@ resource "azurerm_network_interface" "nic" {
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
+
+  depends_on = [
+    azurerm_subnet.subnet,
+    azurerm_public_ip.public_ip,
+    azurerm_network_security_group.nsg
+  ]
+}
+
+resource "azurerm_network_interface_security_group_association" "nsg_association" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+
+  depends_on = [azurerm_network_interface.nic]
 }
 
 resource "azurerm_virtual_machine" "vm" {
@@ -50,7 +91,7 @@ resource "azurerm_virtual_machine" "vm" {
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.nic.id]
-  vm_size               = "Standard_B1ms""Standard_D2s_v3"
+  vm_size               = "Standard_B1ms" #"Standard_D2s_v3"
 
   storage_image_reference {
     publisher = "Canonical"
@@ -69,34 +110,44 @@ resource "azurerm_virtual_machine" "vm" {
   os_profile {
     computer_name  = "casopractico2-lgc2-vm"
     admin_username = "leo_gomez"
-    admin_password = "Rock4YouNot4Me"
+
+    linux_config {
+      disable_password_authentication = true
+      ssh_keys {
+        path     = "/home/leo_gomez/.ssh/authorized_keys"
+        key_data = file("./ssh/authorized_keys")
+      }
+    }
   }
 
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
+  depends_on = [
+    azurerm_network_interface.nic,
+    azurerm_network_interface_security_group_association.nsg_association
+  ]
 }
 
-#resource "azurerm_kubernetes_cluster" "aks" {
-#  name                = "casopractico2-lgc2--aks"
-#  location            = azurerm_resource_group.rg.location
-#  resource_group_name = azurerm_resource_group.rg.name
-#  dns_prefix          = "casopractico2-lgc2--aks"
-#
-#  default_node_pool {
-#    name       = "default"
-#    node_count = 1
-#    vm_size    = "Standard_D2s_v3"
-#  }
-#
-#  identity {
-#    type = "SystemAssigned"
-#  }
-#
-#  network_profile {
-#    network_plugin    = "azure"
-#    load_balancer_sku = "standard"
-#  }
-#
-#}
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "casopractico2-lgc2--aks"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "casopractico2-lgc2--aks"
 
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2s_v3"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin    = "azure"
+    load_balancer_sku = "standard"
+  }
+
+  depends_on = [
+    azurerm_virtual_machine.vm
+  ]
+}
